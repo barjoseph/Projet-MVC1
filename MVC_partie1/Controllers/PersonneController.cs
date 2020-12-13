2d0cc9ecdb6373b6f1d28560f03c599558f7aa3a
barjoseph <bardancajoseph2030@gmail.com>
2020-12-13 17:39:05 +0100

    Premiere sauvegarde MVC_partie1

---
+++ MVC_partie1/Controllers/PersonneController.cs
@@ -0,0 +1,38 @@
+﻿using MVC_partie1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_partie1.Controllers
+{
+    public class PersonneController : Controller
+    {
+        // GET: Personne
+        public ActionResult Personne()
+        {
+            
+            Personne obj = new Personne();
+            
+            //obj.age = 0;
+            //obj.nom = " ";
+            //obj.prenom = " ";
+            
+                obj.age = Convert.ToInt32(Request.Form["age"]);
+               
+            if (obj.nom!=null)   
+                obj.nom = Request.Form["nom"].ToString();
+
+            if (obj.prenom != null)
+                obj.prenom = Request.Form["prenom"].ToString();
+
+            return View(obj);
+        }
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ MVC_partie1/Models/Personne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_partie1.Models
+{
+    public class Personne
+    {
+
+        public int age { get; set; }
+        public String nom { get; set; }
+        public String prenom { get; set; }
+
+        public Personne()
+        {
+            this.age = 0;
+            this.nom = "";
+            this.prenom = "";
+        }
+    }   
+       
+    
+}
